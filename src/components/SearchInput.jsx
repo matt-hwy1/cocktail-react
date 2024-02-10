3b688437578c231bc55bc66f1f5ea4005a3e5202
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-10 12:50:11 -0700

    Add generic SearchInput autocomplete component

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,22 @@
+import './css/search.css';
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+export const SearchInput = ({ setResults, url }) => {
+  const [query, setQuery] = useState("")
+
+  function handleChange(event) {
+    fetch(`${url}?query=${event.target.value}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setResults(json);
+      });
+  }
+
+  return (
+    <div className="search">
+      <FaSearch />
+      <input className="search-input" placeholder="Cocktail Name" onChange={handleChange} />
+    </div>
+  )
+}
