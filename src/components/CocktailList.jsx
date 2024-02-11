c999c5274ed937e63ad67cc4fe3aea38f44d348d
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-11 08:53:03 -0700

    Add null guard to rendering

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -2,9 +2,10 @@ import { CocktailSummary } from "./CocktailSummary"
 
 export const CocktailList = ({ cocktails }) => {
   return (
+    cocktails["drinks"] != undefined &&
     <div>
       <ul className="search-results">
-          {cocktails["drinks"] && cocktails["drinks"].map((cocktail, id) => {
+          {cocktails["drinks"].map((cocktail, id) => {
             return <CocktailSummary cocktail={cocktail} key={id} />;
           })}
         </ul>
